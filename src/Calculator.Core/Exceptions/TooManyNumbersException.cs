e4f5e43f750b72764ba31d11cd21c00716caeca2
Matheus Costa Vieira <matheus.costa.vieira@gmail.com>
2026-02-02 20:43:18 -0300

    chore: remove unused TooManyNumbersException class

--- src/Calculator.Core/Exceptions/TooManyNumbersException.cs
+++
@@ -1,9 +0,0 @@
-namespace Calculator.Core.Exceptions;
-
-/// <summary>
-/// Exception thrown when more than the maximum allowed numbers are provided (Step 1).
-/// </summary>
-/// <param name="count">The actual count of numbers provided.</param>
-/// <param name="maxAllowed">The maximum allowed numbers (default is 2).</param>
-public class TooManyNumbersException(int count, int maxAllowed = 2) 
-    : Exception($"Expected at most {maxAllowed} numbers, but got {count}.");
